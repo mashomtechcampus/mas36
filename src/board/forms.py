2b7c85b695de1716bb624996dfc3fd8905014906
mashom aljedani <mashom.techcampus@gmail.com>
2019-11-01 23:58:38 +0300

    whatsup + error

--- src/board/forms.py
+++ src/board/forms.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 
 class Creat_form(forms.ModelForm):
     title = forms.CharField(max_length=100,label='عنوان التصميم')
-    img_url = forms.URLField(max_length=100, label='عنوان التصميم')
+    img_url = forms.URLField(max_length=100, label='رابط الصور')
 
 
 
